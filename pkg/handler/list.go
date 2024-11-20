a2c9aec6030b98f9b1524031b2bc43be9e8ce0e8
AndroSaal <89251409398@mail.ru>
2024-11-20 17:47:13 +0300

    add: Implementation for ListCreaate

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,20 +1,37 @@
 package handler
 
 import (
+	todo "ToDoApp"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) createList(c *gin.Context) {
-	id, _ := c.Get(userCtx)
-	c.JSON(http.StatusOK, map[string]interface{}{
-		"id": id,
-	})
+	userId, ok := getUserId(c)
+	if ok != nil {
+		return
+	}
+
+	var input todo.TodoList
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	//call service method
+	id, err := h.services.TodoList.CreateList(userId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"id": id})
+
 }
 
 func (h *Handler) getAllList(c *gin.Context) {
-
+	
 }
 
 func (h *Handler) getListById(c *gin.Context) {
