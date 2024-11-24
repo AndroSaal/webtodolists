a30ed9595b444a5fd205deec3731d66532b37846
AndroSaal <89251409398@mail.ru>
2024-11-24 19:49:28 +0300

    add: new handler to work with items

--- pkg/handler/item.go
+++ pkg/handler/item.go
@@ -1,9 +1,48 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	todo "ToDoApp/entities"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) createItem(c *gin.Context) {
-	
+
+	//для создания задачи нужно получать id пользователя и id списка
+	userId, ok := getUserId(c) //получаем id пользователя из midleware
+	if ok != nil {
+		newErrorResponse(c, http.StatusBadRequest, "This user not found")
+		return
+	}
+
+	//получение id списка
+	listId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, fmt.Sprintf(err.Error(), " This list not found"))
+		return
+	}
+
+	//получение input: из json в struct
+	var input todo.TodoItem
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	//отправляем на уровень ниже - в сервис(в уровень бизнес-логики)
+	id, err := h.services.TodoItem.Create(userId, listId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	//отправка ответа, если все закончилось хорошо
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
+
 }
 
 func (h *Handler) getAllItem(c *gin.Context) {
@@ -19,5 +58,5 @@ func (h *Handler) updateItem(c *gin.Context) {
 }
 
 func (h *Handler) deleteItem(c *gin.Context) {
-	 
+
 }
